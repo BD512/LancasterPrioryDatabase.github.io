f035870cb5ad9450559d596b77c44d24cf1fc574
Beth <bethd835@gmail.com>
2026-08-22 20:25:22 +0100

    Add files via upload

---
+++ SupabaseAccess.js
@@ -0,0 +1,96 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export class SupabaseAccess {
+    constructor(url, key, category_table="tblMatchingCategory", items_table="tblHistoricalItem", images_table="tblItemImage", bucket_name="Images") {
+        this.connection = createClient(url, key)
+        this.items_table = items_table;
+        this.images_table = images_table;
+        this.category_table = category_table;
+        this.bucket_name = bucket_name;
+        this.categories = null;
+        this.subcategories = null;
+    }
+    async initialise(){
+        this.categories = await this.selectCategories()
+        this.subcategories_dict = await this.getSubcategoryDict()
+    }
+    async selectEntries(name=null, category=null, subcategory=null, description_keyword=null, start_date=null, end_date=null) {
+        let query = this.connection.from(this.items_table).select("*");
+        if (name != null) {
+            query = query.ilike("Name", "%"+name+"%");
+        }
+        if (category != null) {
+            query = query.eq("Category", category);
+        }
+        if (subcategory != null) {
+            query = query.eq("Subcategory", subcategory);
+        }
+        if (description_keyword != null) {
+            query = query.ilike("Description", "%"+description_keyword.replace(/ /g, "%")+"%") //
+        }
+        if (start_date != null) {
+            query = query.gte("Year", start_date);
+        }
+        if (end_date != null) {
+            query = query.lte("Year", end_date);
+        }
+        const { data, error } = await query;
+        console.log(error);
+        return data;
+    }
+    async selectCategories() {
+        const {data, error} = await this.connection.from(this.category_table).select("Category");
+        console.log(error);
+        return new Set(data.map(row => row.Category));
+    }
+    async getCategories() {
+        return this.categories;
+    }
+    async getSubcategoryDict() {
+        const {data, error} = await this.connection.from(this.category_table).select("Category, Subcategory");
+        console.log(error);
+        const dict = {};
+        for (const row of data) {
+            const key = row.Category;
+            const value = row.Subcategory;
+            if (!dict[key]) {
+                dict[key] = [];
+            }
+            dict[key].push(value);
+        }
+        return dict;
+    }
+    async getSubcategoriesForCategory(category=null) {
+        // console.log(this.subcategories_dict)
+        // console.log(this.subcategories_dict[category])
+        return this.subcategories_dict[category];
+    }
+    async getImagePaths(item_id) {
+        console.log(item_id);
+        console.log(this.images_table);
+        const {data, error} = await this.connection.from(this.images_table).select("IDNumber, ImagePath").eq("IDNumber", item_id);
+        // console.log(error);
+        console.log("Data", data);
+        return new Set(data.map(row => row.ImagePath));
+    }
+    async loadImage(name) {
+        const cached = sessionStorage.getItem(name);
+        if (cached) {
+            const img = new Image();
+            img.src = cached;
+            return img;
+        }
+        const url = this.connection.storage
+            .from(this.bucket_name)
+            .getPublicUrl(name).data.publicUrl;
+        sessionStorage.setItem(name, url);
+        const img = new Image();
+        img.src = url;
+        return img;
+    }
+
+}
+const database = new SupabaseAccess("https://ynrxfhpltaivjwbfsufa.supabase.co", "sb_publishable_Z-TIgzIUXW7La2XHy5upVg_DvLV0sKG");
+
+// window.database = database;
+console.log(await database.selectCategories());
---
+++ app.js
@@ -0,0 +1,109 @@
+import { SupabaseAccess }
+    from "./SupabaseAccess.js";
+
+const database = new SupabaseAccess("https://ynrxfhpltaivjwbfsufa.supabase.co", "sb_publishable_Z-TIgzIUXW7La2XHy5upVg_DvLV0sKG");
+await database.initialise()
+
+async function showCategories(){
+    const categories = await database.selectCategories()
+    //console.log(categories)
+    const dropdown_select = document.getElementById("category");
+    //console.log("here");
+    categories.forEach(category => {
+        //console.log('here');
+        const option =
+            document.createElement(
+                "option"
+            );
+        option.value = category;
+        option.textContent = category;
+        dropdown_select.appendChild(option);
+    });
+}
+
+async function showSubcategories(){
+    const category_select = document.getElementById("category");
+    const subcategory_select = document.getElementById("subcategory");
+    const category = category_select.options[category_select.selectedIndex].value
+    let subcategories = ["All Subcategories"];
+    if (category !== ""){
+        subcategories = subcategories.concat(await database.getSubcategoriesForCategory(category));
+        // console.log("here");
+        // console.log(subcategories);
+    }
+    subcategory_select.options.length = 0;
+    subcategories.forEach(subcategory => {
+        //console.log('here');
+        const option =
+            document.createElement(
+                "option"
+            );
+        option.value = subcategory;
+        option.textContent = subcategory;
+        subcategory_select.appendChild(option);
+    });
+}
+
+async function showTable(){
+    let name = document.getElementById("name").value || null;
+    let description = document.getElementById("description").value || null;
+    let category = document.getElementById("category").value || null;
+    let subcategory = document.getElementById("subcategory").value || null;
+    let start_date = document.getElementById("start_year").value || null;
+    let end_date = document.getElementById("end_year").value || null;
+    // console.log(name, description, category, start_date, end_date);
+    if (category === "" || category === "All Categories"){
+        category = null;
+    } if (subcategory === "All Subcategories" || subcategory === ""){
+        subcategory = null;
+    }
+    let entries = await database.selectEntries(name, category, subcategory, description, start_date, end_date);
+    // console.log(entries)
+    const table_body = document.getElementById("historical_data");
+    table_body.innerHTML = "";
+    for (const entry of entries){
+        const row = document.createElement("tr");
+        const name_cell = document.createElement("td");
+        name_cell.textContent = entry.Name.toUpperCase();
+        row.appendChild(name_cell);
+        const description_cell = document.createElement("td");
+        description_cell.textContent = entry.Description;
+        row.appendChild(description_cell);
+        const category_cell = document.createElement("td");
+        category_cell.textContent = entry.Category;
+        row.appendChild(category_cell);
+        const subcategory_cell = document.createElement("td");
+        subcategory_cell.textContent = entry.Subcategory;
+        row.appendChild(subcategory_cell);
+        const year_cell = document.createElement("td");
+        console.log(entry.Confident);
+        if (entry.Confident === false) {
+            year_cell.textContent = "c.".concat(" ", entry.Year);
+        } else{
+            year_cell.textContent = entry.Year;
+        }
+        // year_cell.textContent = entry.Year;
+        row.appendChild(year_cell);
+        const images_cell = document.createElement("td");
+        const images = await database.getImagePaths(entry.IDNumber);
+        console.log(images);
+        for (const path of images) {
+            const img = await database.loadImage(path);
+            img.classList.add("item-image");
+            img.alt = entry.Name;
+            images_cell.appendChild(img);
+        }
+        row.appendChild(images_cell);
+        table_body.appendChild(row);
+
+    }
+
+}
+
+await showCategories();
+await showTable();
+// console.log((await database.getSubcategoryDict())["Stained Glass Windows"]);
+// var a = await database.getSubcategoriesForCategory("Stalls");
+// console.log(a);
+document.getElementById("category").addEventListener("change", showSubcategories);
+document.getElementById("search").addEventListener("click", showTable);
