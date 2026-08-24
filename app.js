db61fcdb58887c8499f06afc944b25d5045a6eb8
BD512 <bethd835@gmail.com>
2026-08-24 10:28:10 +0100

    Made image loading not lazy and added test.

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ async function showCategories(){
 }
 
 async function showSubcategories(){
+    console.log("Updating subcategories");
     const category_select = document.getElementById("category");
     const subcategory_select = document.getElementById("subcategory");
     const category = category_select.options[category_select.selectedIndex].value
@@ -91,7 +92,7 @@ async function showTable(){
             const img = await database.loadImage(path);
             img.classList.add("item-image");
             img.alt = entry.Name;
-            img.loading = "lazy";
+            // img.loading = "lazy";
             images_cell.appendChild(img);
         }
         row.appendChild(images_cell);
