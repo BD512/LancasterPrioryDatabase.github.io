699a6288bb84ea99e645d38b791d405acacd3526
BD512 <bethd835@gmail.com>
2026-08-24 10:21:30 +0100

    Made image loading lazy.

--- app.js
+++ app.js
@@ -91,6 +91,7 @@ async function showTable(){
             const img = await database.loadImage(path);
             img.classList.add("item-image");
             img.alt = entry.Name;
+            img.loading = "lazy";
             images_cell.appendChild(img);
         }
         row.appendChild(images_cell);
